0170920dc8c0a6fdd08a620ba934f6beacacd532
Igor Gonchar <IgorGoncharUA@gmail.com>
2018-11-12 23:26:46 +0200

    login validation was added

--- AngularApp/src/app/login/login.component.ts
+++ AngularApp/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Location} from '@angular/common';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,37 @@ import {Location} from '@angular/common';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor(private location: Location) {
+  constructor(private location: Location,
+              private fb: FormBuilder) {
   }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      login: ['', [
+        Validators.required
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(5)
+      ]]
+    });
+  }
+
+  get login() {
+    return this.loginForm.get('age');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  onSubmit() {
+    console.log('Logged in');
   }
 }
